7f2b7b3871861d03cf2cd326002936324818e208
Samuel Dost <samuel.dost@gmail.com>
2016-02-17 08:18:13 -0500

    Fix response payload.

--- test/controllers/v1/player_controller_test.exs
+++ test/controllers/v1/player_controller_test.exs
@@ -29,20 +29,17 @@ defmodule GameService.V1.PlayerControllerTest do
     conn = post conn, v1_player_path(conn, :process_request), @valid_params
     assert json_response(conn, 200) == %{
       "player" => %{
-        "social_id" => "9999999999999999",
         "puzzle_data" => %{
           "puzzles" => [1, 2, 3],
           "scores" => [500, 500, 500],
           "stars" => [3, 1, 1]
         },
-        "name" => "Real Name",
         "high_scores" => %{
           "scores" => [],
           "puzzles" => [],
           "leaders" => []
         },
         "friends" => [],
-        "email" => "real@email.com"
       }
     }
   end
--- web/views/v1/player_view.ex
+++ web/views/v1/player_view.ex
@@ -1,12 +1,9 @@
 defmodule GameService.V1.PlayerView do
   use GameService.Web, :view
 
-  def render("player.json", %{player: player, puzzle_data: puzzle_data, friend_data: friend_data, high_scores: high_scores}) do
+  def render("player.json", %{puzzle_data: puzzle_data, friend_data: friend_data, high_scores: high_scores}) do
     %{
       player: %{
-        social_id: player.social_id,
-        name: player.name,
-        email: player.email,
         puzzle_data: puzzle_data,
         friends: friend_data,
         high_scores: high_scores
